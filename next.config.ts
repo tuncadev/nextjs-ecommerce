adad296bfe501eb0700bdb791ae70007ca483fa6
Ozgur Murat Tunca <tunca.development@gmail.com>
2025-11-28 09:30:34 +0200

    config

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,19 @@
 import type { NextConfig } from "next";
-const withPWA = require('next-pwa')({
-  dest: 'public',
+const withPWA = require("next-pwa")({
+  dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
+  disable: process.env.NODE_ENV === "development",
 });
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'backend.tunca.local',
-      },
-      {
-        protocol: 'https',
-        hostname: 'backend.tunca.site',
-      }
+      { protocol: "http", hostname: "backend.tunca.local" },
+      { protocol: "https", hostname: "backend.tunca.site" },
     ],
   },
   compress: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default withPWA(nextConfig);
